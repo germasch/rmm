cbfe7f9fc006fbe67a8b6a24efe31227829de9f3
Mike Wilson <knobby@burntsheep.com>
2020-09-23 18:08:22 +0000

    Adding tracking resource adaptor and limiting resource adaptor. These adaptors will track allocations to detect leaks and limit the GPU allocations available to the process, respectively.
    Fixed a small copy/paste issue inside logging_resource_adaptor as well.

--- tests/mr/device/mr_tests.cpp
+++ tests/mr/device/mr_tests.cpp
@@ -28,7 +28,9 @@ INSTANTIATE_TEST_CASE_P(ResourceTests,
                         ::testing::Values(mr_factory{"CUDA", &make_cuda},
                                           mr_factory{"Managed", &make_managed},
                                           mr_factory{"Pool", &make_pool},
-                                          mr_factory{"Binning", &make_binning}),
+                                          mr_factory{"Binning", &make_binning},
+                                          mr_factory{"Limiting", &make_limiting},
+                                          mr_factory{"Tracking", &make_tracking}),
                         [](auto const& info) { return info.param.name; });
 
 TEST(DefaultTest, CurrentDeviceResourceIsCUDA)
